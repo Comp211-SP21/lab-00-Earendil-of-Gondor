efce6ed72b8c18b0e13d932897f031815cbbf238
illuminator <2642503302@qq.com>
2021-02-01 16:36:27 +0800

    will it work?

---
+++ 0-hello-world/hello.c
@@ -0,0 +1,14 @@
+// PID: 730404996 
+// I pledge the COMP211 honor code.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("hello, world");
+    printf("\n");
+    printf("welcome to c!");
+    printf("\n");
+    return EXIT_SUCCESS;
+}
